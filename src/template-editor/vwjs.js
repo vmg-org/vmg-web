7117c608a342652e091b234161058c66dd9f0201
IvanRave <reyvart@hotmail.com>
2014-10-07 17:03:53 +0400

    feat(templateEditor): check inputs; show tooltips
    create one scope with limits; check episode name and show tooltip
    
    time: 0h:20

--- src/template-editor/vwjs.js
+++ src/template-editor/vwjs.js
@@ -21,6 +21,27 @@ exports.run = function(app) {
       // todo #31! add others
   };
 
+  var inpLimit = {
+    name_of_movie: {
+      max_length: 50,
+      min_length: 3,
+      required: true
+    },
+    name_of_episode: {
+      max_length: 100,
+      min_length: 5,
+      required: true
+    },
+    story_of_episode: {
+      max_length: 400,
+      min_length: 100,
+      required: true
+    },
+    conds_of_episode: {
+      max_length: 100
+    }
+  };
+
   app.onSelectGenre = function(elem, e, heroScopeName, animalScopeName) {
     console.log('app invoked', elem);
     if (elem.value === 'hero') {
@@ -64,12 +85,12 @@ exports.run = function(app) {
     // how to get it?
     // This data stores on model with other props, like name, story
     // a radio button list sends a value to the function (it is like id)
-    var maxLength = 50;
-    var minLength = 3;
+    //    var maxLength = 50;
+    //    var minLength = 3;
     //    var required = true;
     //    var rgx = /\w+/g;
     //    var defaultValue = 'Best movie in the world'; // put to default or placeholder
-    if ((elem.value.length > maxLength) || (elem.value.length < minLength)) {
+    if ((elem.value.length > inpLimit.name_of_movie.max_length) || (elem.value.length < inpLimit.name_of_movie.min_length)) {
       // todo #44! show popup with limits
     } else {
       // hide popup
@@ -78,18 +99,27 @@ exports.run = function(app) {
     app.crtScope.name_of_movie = elem.value;
   };
 
-  app.checkInputNameOfEpisode = function(elem, e, helpElemName) {
-    var maxLength = 30,
-      minLength = 3;
+  app.showTipNameOfEpisode = function(elem, e, targetName) {
+    $('.' + targetName + ':eq(' + (parseInt(elem.getAttribute('data-bind')) - 1) + ')').show().delay(1500).hide('fast');
+  };
 
+  app.showTipStoryOfEpisode = function(elem, e, targetName) {
+    $('.' + targetName).show().delay(1500).hide('fast');
+  };
+
+  app.showTipCondsOfEpisode = function(elem, e, targetName) {
+    $('.' + targetName).show().delay(1500).hide('fast');
+  };
+
+  app.checkInputNameOfEpisode = function(elem, e, helpElemName) {
 
     var orderNumber = parseInt(elem.getAttribute('data-bind'));
     console.log(orderNumber);
-    var helpElems = $('.' + helpElemName);
+    var helpElems = $('.' + helpElemName + ':eq(' + (orderNumber - 1) + ')');
     //var helpElem = $('.' + helpElemName + ':eq(1)');
-    console.log('helpElem', helpElems);
+    //    console.log('helpElem', helpElems);
 
-    if ((elem.value.length > maxLength) || (elem.value.length < minLength)) {
+    if ((elem.value.length > inpLimit.name_of_episode.max_length) || (elem.value.length < inpLimit.name_of_episode.min_length)) {
       helpElems.show();
       // change colors - ? no
       // show ex sign with onhover(onclick) event with hover box with description
@@ -137,20 +167,46 @@ exports.run = function(app) {
     dhr.impl(bem, targetName, mdlName, tags);
   };
 
+  var constructTip = function(limitKey) {
+    var a = inpLimit[limitKey];
+
+    var arrStr = [];
+
+    if (a.max_length) {
+      arrStr.push('Max length: ' + a.max_length);
+    }
+    if (a.min_length) {
+      arrStr.push('Min length: ' + a.min_length);
+    }
+
+    if (a.required) {
+      arrStr.push('Required field');
+    }
+
+    return arrStr.join('<br>');
+  };
+
   app.loadCrtEpisodes = function(elem, e, targetName) {
     console.log('load episodes', targetName);
+
     var data = [{
       order: '1',
       name_order: 'First episode',
       ph_name: 'Episode name',
       ph_story: 'Episode story',
-      ph_conds: 'Episode conditions'
+      ph_conds: 'Episode conditions',
+      tooltip_name: constructTip('name_of_episode'),
+      tooltip_story: constructTip('story_of_episode'),
+      tooltip_conds: constructTip('conds_of_episode')
     }, {
       order: '2',
       name_order: 'Second episode',
       ph_name: 'Episode name',
       ph_story: 'Episode story',
-      ph_conds: 'Episode conditions'
+      ph_conds: 'Episode conditions',
+      tooltip_name: constructTip('name_of_episode'),
+      tooltip_story: constructTip('story_of_episode'),
+      tooltip_conds: constructTip('conds_of_episode')
     }];
 
     var mdlName = 'crt_episode';
