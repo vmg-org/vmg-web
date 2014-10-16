bf76652d5efdcd8f0f5f478b580d399b24daaca0
IvanRave <reyvart@hotmail.com>
2014-10-17 01:01:18 +0400

    feat(watchPage): build a player
    using demo data; videojs player; test in browsers
    
    time: 0h:50

--- src/watch/vwjs.js
+++ src/watch/vwjs.js
@@ -37,17 +37,39 @@ exports.run = function(app) {
   };
 
   app.fillMovie = function(elem, e, targetNameInfo, targetNamePlayer) {
-
     var movieRecordData = [{
       movie_title: 'Big rabbit',
       movie_description: 'eat a cucumber',
-      video_url: './demo.webm'
+      video_url: 'https://s3.amazonaws.com/vmg-bucket/converted/966808401-web.mp4'
     }];
+
     var mdlName = 'movie_record';
     //    var key = 'movie-info-cover';
-    console.log(targetNamePlayer);
-    dhr.impl(bem, targetNamePlayer, mdlName, movieRecordData);
+    //    console.log(targetNamePlayer);
+    //    dhr.impl(bem, targetNamePlayer, mdlName, movieRecordData);
+
+    var videoElem = document.createElement('video');
+    var videoSource = document.createElement('source');
+    videoSource.src = 'https://s3.amazonaws.com/vmg-bucket/converted/966808401-web.mp4';
+    videoSource.type = 'video/mp4';
+    $(videoElem).addClass('video-js vjs-default-skin');
+    videoElem.appendChild(videoSource);
+    $('.' + targetNamePlayer).html(videoElem);
+    // Player builds using videojs and inserted a link
+    window.videojs(videoElem, {
+      width: '100%',
+      height: '100%',
+      controls: true
+    }, function() {
+//      $('.' + targetNamePlayer).show();
+      console.log('player is loaded');
+      // This is functionally the same as the previous example.
+    });
+
+    // Info builds using structure
     dhr.impl(bem, targetNameInfo, mdlName, movieRecordData);
+
+
     //    var key = 'movie-player-cover';
   };
 };
