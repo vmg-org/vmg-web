ee2977376efdbf1f7bded8b37f3e49ed3c9a44fd
IvanRave <reyvart@hotmail.com>
2014-10-17 11:36:41 +0400

    feat(uploading): restructure, limits, progress
    add a size limit, progress counters, re-structure handlers
    
    time: 2h:40

---
+++ src/upload/file-handler.js
@@ -0,0 +1,68 @@
+/**
+ * Handle files from input field (or drag and drop)
+ *     Retrieves signature and other fields from server
+ *     and uploads a file to a cloud
+ *     Returns status of upload
+ * @module
+ */
+'use strict';
+
+var credUpload = require('./cred-upload');
+
+var handleProgress = function(evt) {
+  var pr = parseInt((evt.loaded / evt.total) * 100);
+  var uplPlayer = 'upl-player';
+  $('.' + uplPlayer).html('Loading, % ' + pr + '<br>Loaded, KB: ' + parseInt(evt.loaded / 1024) +
+    ' <br>Total, KB: ' + parseInt(evt.total / 1024));
+};
+
+var cbkFileUpload = function(jobSource, next, files, rejected) {
+  console.log('send begin', jobSource, rejected);
+  //  return;
+  window.FileAPI.upload({
+    url: jobSource.url_to_upload,
+    data: JSON.parse(jobSource.url_to_read),
+    files: {
+      file: files[0]
+    },
+    cache: true, // remove query string from a request - it is required for S3
+    complete: next.bind(null, jobSource),
+    progress: handleProgress
+  });
+};
+
+var cbkCredUpload = function(files, next, err, jobSource) {
+  if (err) {
+    alert('credError');
+    console.log(err);
+    return;
+  }
+
+  if (!jobSource) {
+    alert('noSuchJob');
+    return;
+  }
+
+  window.FileAPI.filterFiles(files, function(file, info /**Object*/ ) {
+    console.log(info);
+    return true;
+  }, cbkFileUpload.bind(null, jobSource, next));
+};
+
+/**
+ * Handle
+ * @param {Object} evt - event from input selector or drag and drop
+ */
+exports.handleFileSelector = function(next, evt) {
+  var files = window.FileAPI.getFiles(evt); // Retrieve file list
+  var needFile = files[0];
+  console.log(needFile);
+  console.log('evt', evt);
+  var demoIdOfMediaSpec = parseInt(window.location.hash.substr(1));
+  credUpload.run({
+    id_of_media_spec: demoIdOfMediaSpec, //from hash
+    id_of_container_format: needFile.type
+  }, cbkCredUpload.bind(null, files, next));
+};
+
+module.exports = exports;
--- src/upload/vwjs.js
+++ src/upload/vwjs.js
@@ -1,53 +1,41 @@
 /** @module upload/vwjs */
 'use strict';
 
-
-var fileUploader = require('./file-uploader');
 var dhr = require('../vmg-helpers/dom');
 var demoBid = require('./demo-bid');
-var credUpload = require('./cred-upload');
+var fileHandler = require('./file-handler');
+var jobSourceChecker = require('./job-source-checker');
 
-var cbkFileUpload = function(jobSource, files, rejected) {
-  console.log('send begin', jobSource);
-  //  return;
-  window.FileAPI.upload({
-    url: jobSource.url_to_upload,
-    data: JSON.parse(jobSource.url_to_read),
-    files: {
-      file: files[0]
-    },
-    cache: true // remove query string from a request - it is required for S3
+var handleJob = function(uplPlayer, err, mediaFile) {
+  console.log('handleJob');
+  if (err) {
+    console.log('error', err);
+    return alert(err.message);
+  }
+  var videoElem = document.createElement('video');
+  var videoSource = document.createElement('source');
+  videoSource.src = mediaFile.url;
+  videoSource.type = 'video/mp4';
+  $(videoElem).addClass('video-js vjs-default-skin');
+  videoElem.appendChild(videoSource);
+  $('.' + uplPlayer).html(videoElem);
+  // Player builds using videojs and inserted a link
+  window.videojs(videoElem, {
+    width: '100%',
+    height: '100%',
+    controls: true
+  }, function() {
+    console.log('player is loaded');
   });
 };
 
-var cbkCredUpload = function(files, err, jobSource) {
-  if (err) {
-    alert('credError');
-    console.log(err);
-    return;
-  }
+var handleResultOfUpload = function(jobSource, err, xhr) {
+  console.log('resultofupload', jobSource, err, xhr);
 
-  if (!jobSource) {
-    alert('noSuchJob');
-    return;
-  }
-
-  window.FileAPI.filterFiles(files, function(file, info /**Object*/ ) {
-    console.log(info);
-    return true;
-  }, cbkFileUpload.bind(null, jobSource));
-};
-
-var handleFileSelector = function(evt) {
-  var files = window.FileAPI.getFiles(evt); // Retrieve file list
-  var needFile = files[0];
-  console.log(needFile);
-  console.log('evt', evt);
-  var demoIdOfMediaSpec = parseInt(window.location.hash.substr(1));
-  credUpload.run({
-    id_of_media_spec: demoIdOfMediaSpec, //from hash
-    id_of_container_format: needFile.type
-  }, cbkCredUpload.bind(null, files));
+  // TODO: #32! From markup
+  var uplPlayer = 'upl-player';
+  $('.' + uplPlayer).html('Conversion started...');
+  jobSourceChecker.run(jobSource, handleJob.bind(null, uplPlayer));
 };
 
 // bem - second
@@ -56,17 +44,20 @@ exports.run = function(app) {
     console.log('fire', fileSelectorName);
     // todo: #43! disable from double click
     var fileSelectorElem = dhr.getElem('.' + fileSelectorName);
-    window.FileAPI.event.on(fileSelectorElem, 'change', handleFileSelector);
+    window.FileAPI.event.on(fileSelectorElem, 'change', fileHandler.handleFileSelector.bind(null, handleResultOfUpload));
     // attach event and fired
 
     dhr.trigger('.' + fileSelectorName, 'click');
   };
+
   // after selection of a file
   app.handleUpload = function(elem) {
-    console.log('handle upload', elem.files);
+    //    console.log('handle upload', elem.files);
+    var uplSelector = elem.getAttribute('data-selector');
+    dhr.hideElems('.' + uplSelector);
     //console.log(elem.files);
-    //    var uplPlayer = elem.getAttribute('data-player');
-    //    var uplSelector = elem.getAttribute('data-selector');
+    var uplPlayer = elem.getAttribute('data-player');
+    dhr.showElems('.' + uplPlayer);
     //
     //    fileUploader.run(elem.files[0], uplPlayer, uplSelector);
   };
@@ -74,17 +65,17 @@ exports.run = function(app) {
   app.handleDropFile = function(elem, e) {
     e.stopPropagation();
     e.preventDefault();
-
-    var dt = e.dataTransfer;
-    var files = dt.files;
-    console.log(files);
-
-    //  'data-player': 'upl-player', // show it
-    //  'data-selector': 'upl-selector' // hide it			 
-    var uplPlayer = elem.getAttribute('data-player');
-    var uplSelector = elem.getAttribute('data-selector');
-
-    fileUploader.run(files[0], uplPlayer, uplSelector);
+    // TODO: 43! Using FILE API
+    //    var dt = e.dataTransfer;
+    //    var files = dt.files;
+    //    console.log(files);
+    //
+    //    //  'data-player': 'upl-player', // show it
+    //    //  'data-selector': 'upl-selector' // hide it			 
+    //    var uplPlayer = elem.getAttribute('data-player');
+    //    var uplSelector = elem.getAttribute('data-selector');
+    //
+    //    fileUploader.run(files[0], uplPlayer, uplSelector);
   };
 
   app.handleDragEnterFile = function(elem, e) {
