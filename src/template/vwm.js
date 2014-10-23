4ebf7c02849545a29182aff46ebf8fed163b7b12
IvanRave <reyvart@hotmail.com>
2014-10-23 10:19:21 +0400

    refactor(movieTemplate): data model

--- src/template/vwm.js
+++ src/template/vwm.js
@@ -22,8 +22,9 @@ exports.loadUserRights = function(next) {
 
 exports.fillMovieTemplate = function(next) {
   dhr.impl(this.bem, this.cls.movieTemplateScope, 'movie_template', [this.movieTemplate]);
-  dhr.impl(this.bem, this.cls.genreTagScope, 'genre_tag', [this.movieTemplate.genre_tag_item]);
-  dhr.impl(this.bem, this.cls.episodeTemplateScope, 'episode_template', this.movieTemplate.episode_template_arr);
+  dhr.impl(this.bem, this.cls.genreTagScope, 'genre_tag', [this.movieTemplate.movie_genre_item.genre_tag_item]);
+  // TODO: #33! episodes - later in next request
+  //  dhr.impl(this.bem, this.cls.episodeTemplateScope, 'episode_template', this.movieTemplate.episode_template_arr);
   next();
 };
 
--- src/vmg-services/movie-template.js
+++ src/vmg-services/movie-template.js
@@ -9,6 +9,7 @@ exports.getListOfMovieTemplate = function(next) {
   next(null, ['asdfasd']);
 };
 
+// {"id":3333,"name":"MyMovie","duration_of_episodes":15,"preview_img_url":"","created":1414043719,"finished":1414304719,"movie_genre_item":{"id_of_movie_template":3333,"id_of_genre_tag":"nature","color_schema":"superschemaforpickcolor","genre_tag_item":{"id":"nature","name":"Nature","color":"green"}}} 
 var handleMovieTemplateWithEpisodes = function(next, err, data) {
   if (err) {
     return next(err);
@@ -19,7 +20,7 @@ var handleMovieTemplateWithEpisodes = function(next, err, data) {
   data.duration_of_episodes_str = data.duration_of_episodes + ' seconds';
   data.created_str = ahr.getTimeStr(data.created, 'lll');
   data.finished_str = ahr.getTimeStr(data.finished, 'lll');
-  data.genre_tag_item.style = 'color: ' + data.genre_tag_item.color;
+  data.movie_genre_item.genre_tag_item.style = 'color: ' + data.movie_genre_item.genre_tag_item.color;
   //  ahr.each(data.episode_templates, function(item) {
   //    item.name_order = 'Episode ' + item.order_in_movie;
   //  });
