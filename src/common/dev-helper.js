c9ce5d2e225601ad4d9cf65c4c7aa261918e5523
IvanRave <reyvart@hotmail.com>
2014-10-31 11:19:54 +0300

    feat(dev): select a dev user to login
    point a number
    
    time: 0h:15

--- src/common/dev-helper.js
+++ src/common/dev-helper.js
@@ -2,12 +2,22 @@
 'use strict';
 var srv = require('../vmg-services/srv');
 var dhr = require('../vmg-helpers/dom');
+var ahr = require('../vmg-helpers/app');
 
 var devSignIn = function(next) {
-  srv.w2001({
-    id_of_auth_issuer: 'dev',
-    social_token: 'dev_token'
-  }, next);
+  var userNumber = window.prompt('User # (from 1 to 9):');
+  if (!userNumber) {
+    alert('required: user #');
+    return;
+  }
+
+  userNumber = ahr.toInt(userNumber);
+  if (userNumber >= 1 && userNumber <= 9) {
+    srv.w2001({
+      id_of_auth_issuer: 'dev',
+      social_token: 'snid' + userNumber
+    }, next);
+  }
 };
 
 exports.init = function(next) {
